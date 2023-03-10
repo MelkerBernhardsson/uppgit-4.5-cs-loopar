b85d957052d940cf8f3b9363f86bf17fcdfc6ab0
Admin <Admin@TEB123004>
2023-03-10 09:18:19 +0100

    Add project files.

---
+++ uppgit 4.5 updated/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal");
+            string heltal = Console.ReadLine();
+            for (int I = 0; I < heltal.Length; I++)
+            {
+                string tecken = heltal[I].ToString();
+                int siffra = int.Parse(tecken);
+                int printer = siffra + 1;
+                if (siffra + 1 >= 10)
+                    printer = siffra - 9;
+                Console.Write(printer);
+            }
+        }
+    }
+}
